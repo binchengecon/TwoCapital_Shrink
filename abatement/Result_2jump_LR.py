711062baa0a5fa86b18521191ee7c29a06476059
bcheng4 <bcheng4@mfe02.chicagobooth.edu>
2022-08-16 09:40:22 -0500

    new commit

--- abatement/Result_2jump_LR.py
+++ abatement/Result_2jump_LR.py
@@ -570,7 +570,7 @@ def graph2(psi_0_grid,psi_1_grid,Ig_initial = 1/120):
         for k in range(len(psi_0_grid)):
 
             # plt.plot(res[k]["years"], (res[k]["x"]/(alpha*np.exp(res[k]["states"][:,0])))*100,label=r'$\psi_0=$'+str(psi_0_meshgrid_1d[k])+'$\psi_1=$'+str(psi_1_meshgrid_1d[k]),color="C3")
-            plt.plot(res[k]["years"], (res[k]["x"]/(alpha*np.exp(res[k]["states"][:,0])))*100,label='$\psi_0=${:.3f},$\psi_1=${:.3f}' .format(psi_0_grid[k],psi_1_grid[k])  )
+            plt.plot(res[k]["years"], (res[k]["x"]/(alpha*np.exp(res[k]["states"][:,0])))*100,label='$\psi_0=${:.3f},$\psi_1=${:.1f}' .format(psi_0_grid[k],psi_1_grid[k])  )
             plt.grid(linestyle=':')
             plt.xlabel('Years')
             plt.ylabel('$\%$ of GDP')
