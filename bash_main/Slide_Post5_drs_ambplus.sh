79cf8cd251d7a26826902ac1e3ebe068953ecad6
bcheng4 <bcheng4@mfe01.chicagobooth.edu>
2023-01-21 12:41:54 -0600

    new commit

--- bash_main/Slide_Post5_drs_ambplus.sh
+++ bash_main/Slide_Post5_drs_ambplus.sh
@@ -45,8 +45,20 @@ Xmaxarr=(9.00 4.0 6.0 3.0)
 # xi_a=(0.0012 0.00125 0.0013 0.0014 0.0015)
 # xi_p=(0.050 0.050 0.050 0.050 0.050)
 
-xi_a=(1000. 0.0002 0.0002 0.0015 0.0008)
-xi_p=(1000. 0.050 0.025 0.050 0.025)
+# xi_a=(1000. 0.0002 0.0002 0.0015 0.0008)
+# xi_p=(1000. 0.050 0.025 0.050 0.025)
+
+# xi_a=(1000. 0.0015 0.0008)
+# xi_p=(1000. 0.050 0.025)
+
+# xi_a=(0.0015 0.0013 0.0011 0.0009 0.0008 0.0007 0.0005 0.0003 0.0002)
+# xi_p=(0.050 0.050 0.050 0.050 0.050 0.050 0.050 0.050 0.050)
+
+# xi_a=(0.0008 0.0007 0.0006 0.0005 0.0004 0.0003 0.0002 0.0001 0.00005)
+# xi_p=(0.025 0.025 0.025 0.025 0.025 0.025 0.025 0.025 0.025)
+
+xi_a=(0.0008)
+xi_p=(0.050)
 
 # xi_a=(0.00002 0.00004 0.00006 0.00008 0.00010 0.00002 0.00004 0.00006 0.00008 0.00010)
 # xi_p=(0.050 0.050 0.050 0.050 0.050 0.025 0.025 0.025 0.025 0.025)
@@ -88,7 +100,7 @@ for epsilon in ${epsilonarray[@]}; do
 
 		# action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilonpost}_ah_drs_less2"
 		# action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilon}_drs_unit_ambplus_calibxia"
-		action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilon}_drs_unit_ambplus_addmiss"
+		action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilon}_drs_unit_ambplus_addmiss2"
 		# action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilon}_ah_drs_less2_addmiss2"
 
 		epsilonarr=(0.1 ${epsilon})
--- bash_main/Slide_Pre5_drs_ambplus.sh
+++ bash_main/Slide_Pre5_drs_ambplus.sh
@@ -21,18 +21,18 @@ declare -A hXarr1=([0]=0.2 [1]=0.2 [2]=0.2)
 declare -A hXarr2=([0]=0.1 [1]=0.1 [2]=0.1)
 declare -A hXarr3=([0]=0.05 [1]=0.05 [2]=0.05)
 # hXarrays=(hXarr1 hXarr2 hXarr3)
-hXarrays=(hXarr1)
+# hXarrays=(hXarr1)
 # hXarrays=(hXarr2)
-# hXarrays=(hXarr3)
+hXarrays=(hXarr3)
 
 # Xminarr=(4.00 0.0 -5.5 0.0)
 # Xmaxarr=(9.00 4.0 0.0 3.0)
 
-Xminarr=(4.00 0.0 1.0 0.0)
-Xmaxarr=(9.00 4.0 6.0 3.0)
+# Xminarr=(4.00 0.0 1.0 0.0)
+# Xmaxarr=(9.00 4.0 6.0 3.0)
 
-# Xminarr=(6.50 1.0 2.0 1.0)
-# Xmaxarr=(7.50 2.0 3.0 1.8)
+Xminarr=(6.50 1.0 2.0 1.0)
+Xmaxarr=(7.50 2.0 3.0 1.8)
 
 # xi_a=(0.0002 0.0002)
 # xi_p=(0.1 0.075)
@@ -54,8 +54,10 @@ Xmaxarr=(9.00 4.0 6.0 3.0)
 # xi_a=(0.0012 0.00125 0.0013 0.0014 0.0015)
 # xi_p=(0.050 0.050 0.050 0.050 0.050)
 
-xi_a=(1000. 0.0002 0.0002 0.0015 0.0008)
-xi_p=(1000. 0.050 0.025 0.050 0.025)
+# xi_a=(1000. 0.0002 0.0002 0.0015 0.0008)
+# xi_p=(1000. 0.050 0.025 0.050 0.025)
+xi_a=(1000. 0.0015 0.0008)
+xi_p=(1000. 0.050 0.025)
 
 # psi0arr=(0.005 0.008 0.010 0.012)
 # psi0arr=(0.005)
@@ -96,7 +98,7 @@ for epsilon in ${epsilonarraypre[@]}; do
 			# action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilonpost}_ah_drs_less2"
 			# action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilonpost}_ah_drs_less2_addmiss2"
 			# action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilonpost}_ah_drs_less2_solve0.2"
-			action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilonpost}_drs_unit_ambplus_addmiss"
+			action_name="2jump_step_${hXarr[0]}_${hXarr[1]}_${hXarr[2]}_LR_${epsilonpost}_drs_unit_ambplus_addmiss_smooth"
 
 			epsilonarr=(0.05 ${epsilon})
 			fractionarr=(0.1 ${epsilon})
