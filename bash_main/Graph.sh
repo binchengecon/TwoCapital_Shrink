bcd3f16bf890c4c983651755f16f3e0447558814
bcheng4 <bcheng4@mfe02.chicagobooth.edu>
2023-02-17 17:26:27 -0600

    new commit

--- bash_main/Graph.sh
+++ bash_main/Graph.sh
@@ -18,11 +18,11 @@ hXarrays=(hXarr1)
 # Xminarr=(4.00 0.0 -5.5 0.0)
 # Xmaxarr=(9.00 4.0 0.0 3.0)
 
-# Xminarr=(4.00 0.0 1.0 0.0)
-# Xmaxarr=(9.00 4.0 6.0 3.0)
+Xminarr=(4.00 0.0 1.0 0.0)
+Xmaxarr=(9.00 4.0 6.0 3.0)
 
-Xminarr=(4.00 0.0 0.0 0.0)
-Xmaxarr=(9.00 4.0 7.0 3.0)
+# Xminarr=(4.00 0.0 0.0 0.0)
+# Xmaxarr=(9.00 4.0 7.0 3.0)
 
 
 xi_a=(0.0004 0.0002 0.0001 0.00005)
@@ -44,7 +44,8 @@ psi2arr=(0.0 0.1 0.2 0.3 0.4 0.5)
 
 
 # python_name_unit="Result_2jump_combine_drs_unit_ambplus_addmiss.py"
-python_name_unit="Result_2jump_UD.py"
+# python_name_unit="Result_2jump_UD.py"
+python_name_unit="Result_2jump_UD2.py"
 # python_name_unit="Result_2jump_combine_drs_unit_ambplus_addmiss_bar.py"
 
 server_name="mercury"
@@ -59,7 +60,7 @@ interp_action_name="2jump_step_0.2_0.2_0.2_LR_0.01"
 fstr_SG="NearestNDInterpolator"
 
 auto=1
-year=25
+year=26
 
 for epsilonpost in ${epsilonarraypost[@]}; do
     for hXarri in "${hXarrays[@]}"; do
@@ -95,7 +96,7 @@ for epsilonpost in ${epsilonarraypost[@]}; do
 #SBATCH --partition=standard
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=0-02:00:00
+#SBATCH --time=0-04:00:00
 
 ####### load modules
 module load python/booth/3.8  gcc/9.2.0
